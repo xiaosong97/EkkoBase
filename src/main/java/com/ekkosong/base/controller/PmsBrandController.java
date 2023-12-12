043ed599c367c17e2380519e4776cd939e0b41ef
小松97 <xiaosong_97@163.com>
2023-12-12 17:31:23 +0800

    mall整合Swagger实现在线API文档

--- src/main/java/com/ekkosong/base/controller/PmsBrandController.java
+++ src/main/java/com/ekkosong/base/controller/PmsBrandController.java
@@ -4,6 +4,8 @@ import com.ekkosong.base.common.CommonPage;
 import com.ekkosong.base.common.CommonResult;
 import com.ekkosong.base.mbg.model.PmsBrand;
 import com.ekkosong.base.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
@@ -17,16 +19,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/brand")
 @Slf4j
+@Api(tags = "品牌管理相关接口")
 public class PmsBrandController {
     @Autowired
     private PmsBrandService brandService;
 
     @GetMapping("/all")
+    @ApiOperation("获取所有品牌列表")
     public CommonResult<List<PmsBrand>> all() {
         return CommonResult.success(brandService.listAllBrand());
     }
 
     @PostMapping
+    @ApiOperation("添加品牌")
     public CommonResult createBrand(@RequestBody PmsBrand pmsBrand) {
         int count = brandService.createBrand(pmsBrand);
         if (1 != count) {
@@ -38,6 +43,7 @@ public class PmsBrandController {
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("更新指定id品牌信息")
     public CommonResult updateBrand(@PathVariable Long id, @RequestBody PmsBrand pmsBrandDto, BindingResult result) {
         log.info("update brand id is {}, brand dto is {}", id, pmsBrandDto);
         int count = brandService.updateBrand(id, pmsBrandDto);
@@ -50,6 +56,7 @@ public class PmsBrandController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("删除指定id的品牌")
     public CommonResult deleteBrand(@PathVariable Long id) {
         log.info("删除品牌，id {}", id);
         int count = brandService.deleteBrand(id);
@@ -62,6 +69,7 @@ public class PmsBrandController {
     }
 
     @GetMapping
+    @ApiOperation("分页查询品牌列表")
     public CommonResult<CommonPage<PmsBrand>> listBrand(
             @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
             @RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize) {
@@ -71,6 +79,7 @@ public class PmsBrandController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("获取指定id的品牌详情")
     public CommonResult<PmsBrand> getBrand(@PathVariable Long id) {
         log.info("根据id查询品牌，id 为 {}", id);
         PmsBrand brand = brandService.getBrand(id);
--- src/main/java/com/ekkosong/base/mbg/model/PmsBrand.java
+++ src/main/java/com/ekkosong/base/mbg/model/PmsBrand.java
@@ -1,49 +1,40 @@
 package com.ekkosong.base.mbg.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 
 public class PmsBrand implements Serializable {
+    @ApiModelProperty(value = "主键ID")
     private Long id;
 
+    @ApiModelProperty(value = "名称")
     private String name;
 
-    /**
-     * 首字母
-     */
+    @ApiModelProperty(value = "首字母")
     private String firstLetter;
 
+    @ApiModelProperty(value = "排序")
     private Integer sort;
 
-    /**
-     * 是否为品牌制造商：0->不是；1->是
-     */
+    @ApiModelProperty(value = "是否为品牌制造商：0->不是；1->是")
     private Integer factoryStatus;
 
+    @ApiModelProperty(value = "是否显示")
     private Integer showStatus;
 
-    /**
-     * 产品数量
-     */
+    @ApiModelProperty(value = "产品数量")
     private Integer productCount;
 
-    /**
-     * 产品评论数量
-     */
+    @ApiModelProperty(value = "产品评论数量")
     private Integer productCommentCount;
 
-    /**
-     * 品牌logo
-     */
+    @ApiModelProperty(value = "品牌logo")
     private String logo;
 
-    /**
-     * 专区大图
-     */
+    @ApiModelProperty(value = "专区大图")
     private String bigPic;
 
-    /**
-     * 品牌故事
-     */
+    @ApiModelProperty(value = "品牌故事")
     private String brandStory;
 
     private static final long serialVersionUID = 1L;
